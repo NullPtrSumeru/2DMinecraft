f908b9b0268b2d0223d50b0494f20487e53fcb28
NullPtrFanfic <nullptrlog@gmail.com>
2023-06-09 20:05:52 +0300

    release4.0

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import pygame
 import random
+import requests
+from io import BytesIO 
 
 pygame.init()
 screen = pygame.display.set_mode((400, 400))
 current_color = (255, 0, 0)
 done = False
+
+pygame.mixer.init()  # Инициализация звукового модуля Pygame
+response = requests.get("https://srv5.onlymp3.to/download?file=39fc39122685e647be90cc126ed7cdf1251003003")
+pygame.mixer.music.load(BytesIO(response.content))  # Загрузка музыкального файла
+pygame.mixer.music.play()  # Воспроизведение музыки
 
 while not done:
     for event in pygame.event.get():
